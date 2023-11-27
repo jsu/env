320d1484359ed9ba3ef4313f081474f1af84ac47
jsu <jsu317@gmail.com>
2023-11-27 20:41:01 +0000

    keys

--- scripts/load_secrets.sh
+++ scripts/load_secrets.sh
@@ -5,15 +5,20 @@ get_secret(){
     echo $(aws secretsmanager get-secret-value --secret-id "${secret_id}" --output json | jq ".SecretString" -r)
 }
 
-export DATAOPS_DSN=$(get_secret "etl/astro/dataops/prod1")
-export DATADEV_DSN=$(get_secret "etl/astro/datadev/system1-ingestor-prod")
-export GOOGLE_SHEETS_CREDS=$(get_secret "dataeng/gcp/etl-reports/gsheet-ingest/serviceaccount")
+export datadev_dsn=$(get_secret "etl/astro/datadev/system1-ingestor-prod")
+export dataops_dsn=$(get_secret "etl/astro/dataops/prod1")
+export datasol_dsn=$(get_secret "etl/astro/datasol/prod1")
+export google_sheets_creds=$(get_secret "dataeng/gcp/etl-reports/gsheet-ingest/serviceaccount")
+export SNOWFLAKE=$(get_secret "snowflake/account=etl_user")
+export snowflake_prometheus=$(get_secret "database/system1/prod/snowflake/deng/prometheus_user")
 
 
 get_snowflake_dsn(){
-    secret=$(get_secret "snowflake/account=etl_user")
-    username=$(echo ${secret} | jq ".username" -r)
-    password=$(echo ${secret} | jq ".password" -r)
+    username=$(echo ${SNOWFLAKE} | jq ".username" -r)
+    password=$(echo ${SNOWFLAKE} | jq ".password" -r)
     echo "snowflake://${username}:${password}@bz56129/ETL_MAIN/PUBLIC?warehouse=S1_ETL"
 }
 export SNOWFLAKE_ETL_DSN=$(get_snowflake_dsn)
+
+
+export snowflake_prometheus_dsn=$(echo ${snowflake_prometheus} | jq ".dsn" -r)
